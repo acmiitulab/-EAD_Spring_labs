fe0df1c75c934227e7ee57a0eacf1244743f38d8
mukhametkaly <60399541+mukhametkaly@users.noreply.github.com>
2021-02-04 01:58:26 +0600

    lab1

---
+++ lab1/src/main/java/com/example/demo/BankService.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+
+public interface BankService {
+    double checkBalance(String cardNumber);
+    boolean withdraw(double amount, String cardNumber);
+    boolean topUp(double amount, String cardNumber);
+    boolean validate(String username, String pinCode);
+}
